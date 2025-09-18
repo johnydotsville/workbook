cf69a9240db888008b5ff204a62999963517e75c
johnydotsville <come.as@you.are>
2025-09-18 14:39:24 +0300

    asdfsdddfsd

--- src/components/NoteView.tsx
+++ src/components/NoteView.tsx
@@ -7,34 +7,32 @@ import { useLocation } from "react-router-dom";
 export function NoteView() {
   const location = useLocation();
   
-  // const [note, setNote] = useState<string>('');
-  // const [noteLoading, setNoteLoading] = useState<boolean>(false);
-  // const [noteLoadingError, setNoteLoadingError] = useState<string | null>(null);
+  const [note, setNote] = useState<string>('');
+  const [noteLoading, setNoteLoading] = useState<boolean>(false);
+  const [noteLoadingError, setNoteLoadingError] = useState<string | null>(null);
 
-  // useEffect(() => {
-  //   (async () => {
-  //     const url = `/konspekt/${location.pathname}/index.md`;
-  //     console.log(url)
-  //     setNoteLoading(true);
-  //     try {
-  //       const note = await fetch(url);
-  //       const noteText = await note.text();
-  //       setNote(noteText);
-  //     } catch(error) {
-  //       setNoteLoadingError((error as Error).message)
-  //     } finally {
-  //       setNoteLoading(false);
-  //     }
-  //   })();
-  // }, [location]);
+  useEffect(() => {
+    (async () => {
+      const url = `${location.pathname}/index.md`;
+      setNoteLoading(true);
+      try {
+        const note = await fetch(url);
+        const noteText = await note.text();
+        setNote(noteText);
+      } catch(error) {
+        setNoteLoadingError((error as Error).message)
+      } finally {
+        setNoteLoading(false);
+      }
+    })();
+  }, [location]);
 
-  // if (noteLoading) return <div>Загрузка конспекта...</div>
-  // if (noteLoadingError) return <div>noteLoadingError</div>
+  if (noteLoading) return <div>Загрузка конспекта...</div>
+  if (noteLoadingError) return <div>noteLoadingError</div>
 
   return (
     <div>
-      {/* <MarkdownBox content={note} basePath={location.pathname} /> */}
-      {location.pathname}
+      <MarkdownBox content={note} basePath={location.pathname} />
     </div>
   )
 }
